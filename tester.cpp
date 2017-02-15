d661b2d1f8d686a4a9d29bb084a6b5b3f69264b5
Sean <dcbird92@gmail.com>
2017-02-15 16:01:49 -0700

    Added a tester class and wrote the food item tests

--- tester.cpp
+++ tester.cpp
@@ -1,14 +1,41 @@
 
-#include "food_item.cpp"
+#include "food_item.h"
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
   std::string UPCTest = "0000000001";
-  std::string nameTest = "chocolate salty balls";
+  std::string nameTest = "Orange";
   int lifeTime = 5;
   int amount = 10;
 
-  food_item::food_item testing(UPCTest, nameTest, lifeTime, amount);
-
+  food_item testing(UPCTest, nameTest, lifeTime, amount);
+  
+  // Checking that the get methods all return the correct info
+  // UPC Test
+  if(testing.getUPC() == "0000000001")
+    {
+    cout << "UPC Testing Passed" << endl;
+    }
+  else cout << "UPC Testing Failed";
+  // Name Test
+  if(testing.getName() == "Orange")
+    {
+      cout << "Get Name Testing Passed" << endl;
+    }
+  else cout << "Get Name Test Failed" << endl;
+  // Shelf Life Test
+  if(testing.getLife() == 5)
+    {
+      cout << "Get Life Test Passed" << endl;
+    }
+  else cout << "Get Life Test Failed" << endl;
+  // Amount Test
+  if(testing.getTotal() == 10)
+    {
+      cout << "Get Total Test Passed" << endl;
+    }
+  else cout << "Get Total Test Passed" << endl;
 }
