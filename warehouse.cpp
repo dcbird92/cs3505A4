c7a02364fdc9b3583999c3da4cbf8880fc211fa2
msorger <msorger@lab2-23.eng.utah.edu>
2017-02-15 15:04:15 -0700

    Added comments to skele recieve request

--- warehouse.cpp
+++ warehouse.cpp
@@ -18,16 +18,30 @@
   // adds a food_item to the list
   void warehouse::receive(food_item stock)
   {
+    //records item and quantity as recieved in correct warehouses inv
     std::map<std::string, std::vector<food_item> >::iterator it = inventory.find(stock.getName());
     it->second.push_back(stock);
+    
+    //To keep track of day of which item will expire:
+    //shelflife + currentDate
 
   }
   
   food_item warehouse::request(food_item sell)
   {
+    //Check if warehouse contains that item
 
+    //if multiple groups of that item exists, remove the one with the 
+    //oldest expiration date
+
+    //check if warehouse contains enough items to fulfill request
+    //if it does
+    //   Remove the item
+    //else
+    //   Remove available items, ignore the rest
 
   }
+
   // Update the shelf life of the product and removes if == 0
   void warehouse::update()
   {
