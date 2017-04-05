5d4e1f199b9b9a1adfaa64642977bae6e2ac844d
msorger <msorger@lab2-23.eng.utah.edu>
2017-04-05 17:11:30 -0600

    Kinda wrote save function, compiles, do not know if it is correct

--- Project1_1/bouncer/bouncer.c
+++ Project1_1/bouncer/bouncer.c
@@ -6,6 +6,33 @@
 #include <libavutil/imgutils.h>
 
 
+//*************************************************
+//this is copied from the tutorial
+void saveFrame(AVFrame *vFrame, int width, int height, int iFrame)
+{
+  FILE *imgFILE;
+  char sizeFile[32];
+  int i;
+
+  //open file
+  sprintf(sizeFile, "frame%d.ppm", iFrame);
+  imgFILE = fopen(sizeFile, "spff");
+  if(imgFILE==NULL)
+    return;
+
+  //write header
+  fprintf(imgFILE, "P6\n%d %d\n255\n", width, height);
+
+  //write pixel data
+  for(i = 0; i < height; i++)
+    fwrite(vFrame->data[0]+i*vFrame->linesize[0],1,width*3,imgFILE);
+
+  //close file
+  fclose(imgFILE);
+  //******************************************************
+
+}
+
 void DrawBall()
 {
   
@@ -19,6 +46,7 @@ int main(int argc, char *argv[])
   AVCodec *vCodec;
   AVFrame *vFrame;
   AVFrame *vFrameRGB;
+  int i, videoStream;
   //register all the codecs
   avcodec_register_all();
 
@@ -36,11 +64,21 @@ int main(int argc, char *argv[])
 	}
 
       //dump information about file onto standard error
-
       av_dump_format(inContext, 0, argv[1], 0);
+      
+      // Find the first video stream
+      videoStream=-1;
+      for(i=0; i<inContext->nb_streams; i++)
+	if(inContext->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
+	  videoStream=i;
+	  break;
+	}
+      if(videoStream==-1)
+	return -1; // Didn't find a video stream
 
       // The pointer to the codec context for the video stream for decoding
-      imgCodex = inContext->streams[0]->codec;
+      // This is deperecated
+      imgCodex = inContext->streams[videoStream]->codec;
      
       //find the deocder for the video stream
       vCodec = avcodec_find_decoder(imgCodex->codec_id);
@@ -58,7 +96,6 @@ int main(int argc, char *argv[])
 	}
       
       //make and allocate memory for frame
-      
       vFrame = av_frame_alloc();
       
       
