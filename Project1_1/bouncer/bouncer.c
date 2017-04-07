948f5a30286c0b4b314b78a9c24dcdcdf6a91570
msorger <msorger@lab1-40.eng.utah.edu>
2017-04-07 13:05:42 -0600

    Progress, not getting packet

--- Project1_1/bouncer/bouncer.c
+++ Project1_1/bouncer/bouncer.c
@@ -8,6 +8,7 @@
 
 //*************************************************
 //this is copied from the tutorial
+/*
 void saveFrame(AVFrame *vFrame, int width, int height, int iFrame)
 {
   FILE *imgFILE;
@@ -15,7 +16,7 @@ void saveFrame(AVFrame *vFrame, int width, int height, int iFrame)
   int i;
 
   //open file
-  sprintf(sizeFile, "frame%d.ppm", iFrame);
+  sprintf(sizeFile, "frame%d.spff", iFrame);
   imgFILE = fopen(sizeFile, "spff");
   if(imgFILE==NULL)
     return;
@@ -29,10 +30,10 @@ void saveFrame(AVFrame *vFrame, int width, int height, int iFrame)
 
   //close file
   fclose(imgFILE);
-  //******************************************************
 
 }
 
+*/
 void DrawBall()
 {
   
@@ -41,14 +42,17 @@ void DrawBall()
 int main(int argc, char *argv[])
 {
   AVPacket imgPack;
-  AVFormatContext *inContext;
-  AVCodecContext *imgCodex;
-  AVCodec *vCodec;
-  AVFrame *vFrame;
-  AVFrame *vFrameRGB;
+  AVFormatContext *inContext = NULL;
+  AVCodecContext *imgCodex = NULL;
+  AVCodec *vCodec = NULL;
+  AVFrame *vFrame = NULL;
+  AVFrame *vFrameRGB = NULL;
+  AVCodecParameters *codecParams; 
   int i, videoStream;
+  FILE *file;
+
   //register all the codecs
-  avcodec_register_all();
+  av_register_all();
 
   if(argc !=2)
     {
@@ -56,54 +60,103 @@ int main(int argc, char *argv[])
     }
   else
     {
-      
       if(avformat_open_input(&inContext, argv[1], NULL, NULL) != 0)
+  	{
+	  printf("Couldn't open file - open_input \n");
+	  return -1;
+	}
+      
+      if(avformat_find_stream_info(inContext, NULL) < 0)
 	{
-	  printf("Couldn't open file");
-	  return 0;
+	  printf("Could not find stream information");
+	  return -1;
 	}
 
       //dump information about file onto standard error
       av_dump_format(inContext, 0, argv[1], 0);
-      
-      // Find the first video stream
+            
+      //Find the first video stream
       videoStream=-1;
       for(i=0; i<inContext->nb_streams; i++)
-	if(inContext->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
-	  videoStream=i;
-	  break;
+	{
+	  if(inContext->streams[i]->codecpar->codec_type==AVMEDIA_TYPE_VIDEO)
+	    {
+	      videoStream=i;
+	      break;
+	    }
 	}
+      
       if(videoStream==-1)
-	return -1; // Didn't find a video stream
+	{
+	  printf("Didn not find a video stream");
+	  return -1;
+	}
+
+      //Allocate Memory for codex context
+      imgCodex = avcodec_alloc_context3(vCodec);
 
       // The pointer to the codec context for the video stream for decoding
-      // This is deperecated
-      imgCodex = inContext->streams[videoStream]->codec;
-     
-      //find the deocder for the video stream
+      codecParams = inContext->streams[videoStream]->codecpar;
+      if(avcodec_parameters_to_context(imgCodex, codecParams) < 0)
+	{
+	  printf("Could not get codexContext from parameters");
+	  return -1;
+	}
+      
+      //find the decoder for the video stream
       vCodec = avcodec_find_decoder(imgCodex->codec_id);
-      if(vCodec->id != AV_CODEC_ID_MJPEG) //warning here, checking if JPG
+      if(vCodec->id != AV_CODEC_ID_MJPEG)
 	{
 	  printf("This is not a Jpg file, cannot decode");
-	  return 0;
+	  return -1;
 	}
-
+      
       //attempt to open codec
       if(avcodec_open2(imgCodex, vCodec, 0) < 0)
 	{
 	  printf("Could not open codec\n");
-	  return 0;
+	  return -1;
 	}
-      
+
+      //When the JPG image data comes in uncompressed, these are the pixelformats that it comes in as.
+      //The pixelformats; however, are deprecated and thus need to be changed to a different one
+      switch (imgCodex->pix_fmt)
+	{
+	case AV_PIX_FMT_YUVJ420P :
+	  imgCodex->pix_fmt = AV_PIX_FMT_YUV420P;
+	  break;
+	case AV_PIX_FMT_YUVJ422P  :
+	  imgCodex->pix_fmt = AV_PIX_FMT_YUV422P;
+	  break;
+	case AV_PIX_FMT_YUVJ444P   :
+	  imgCodex->pix_fmt = AV_PIX_FMT_YUV444P;
+	  break;
+	case AV_PIX_FMT_YUVJ440P :
+	  imgCodex->pix_fmt = AV_PIX_FMT_YUV440P;
+	default:
+	  break;
+	}
+
+      //STORING THE DATA
+
       //make and allocate memory for frame
       vFrame = av_frame_alloc();
-      
-      
+           
       vFrameRGB = av_frame_alloc();
 
       //make the buffer to store data from frame into
-      uint8_t *buffer = (uint8_t *)av_malloc(32*sizeof(uint8_t)); //32 * 8
+      uint8_t *buffer = NULL;
+      int numBytes;
+      //Determine required buffer size and allocate buffer
+      numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGB24, imgCodex->width, imgCodex->height,1);
+      buffer = (uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
       
+      //fill frame with info from copy frame (had to change info to RGB24
+      //this is the non-deprecated version of avpicture_fill().
+      av_image_fill_arrays(vFrameRGB->data,vFrameRGB->linesize, buffer, AV_PIX_FMT_RGB24, imgCodex->width, imgCodex->height,1); 
+      
+      //READING THE DATA
+
       //copy original context into another frame
       struct SwsContext *swsCtx = sws_getContext(imgCodex->width, 
 						 imgCodex->height,
@@ -111,69 +164,90 @@ int main(int argc, char *argv[])
 						 imgCodex->width,
 						 imgCodex->height,
 						 AV_PIX_FMT_RGB24,
-						 0,
+						 SWS_BILINEAR,
 						 NULL,
 						 NULL,
 						 NULL
 						 );
       
-      
-      //perform a pixelcopy for info of copy frame to new frame
-      
-
-      //fill frame with info from copy frame (had to change info to RGB24
-      //this is the non-deprecated version of avpicture_fill().
-      av_image_fill_arrays(vFrameRGB->data,vFrameRGB->linesize, buffer, AV_PIX_FMT_RGB24, imgCodex->width, imgCodex->height,1); 
-    
       av_read_frame(inContext, &imgPack);
-
-      //  avcodec_decode_video2(imgCodex, vFrame,1, &imgPack);
-     
+      
+      //Decode video frame
       avcodec_send_packet(imgCodex,&imgPack);
       avcodec_receive_frame(imgCodex,vFrame);
+
+      //printf(vFrame->data[0]);
+      printf("%p \n", vFrame->linesize);
+      printf("%d \n", imgCodex->height);
+      printf("%p \n", vFrameRGB->linesize);
+
+      //Convert Image from its native format to RGB
       sws_scale(swsCtx,(uint8_t const * const *)vFrame->data,vFrame->linesize,0,imgCodex->height,
-		vFrameRGB->data, vFrameRGB->linesize);
-			   
+		vFrameRGB->data, vFrameRGB->linesize);	
 
-    }
+      // Get the encoder for spff
+      AVCodec *spffCodec = avcodec_find_encoder(AV_CODEC_ID_SPFF);
+      if(!spffCodec)
+	return -1;
+      
+      // create a codec context spff
+      AVCodecContext *spffContext = avcodec_alloc_context3(spffCodec);
+      if(!spffContext)
+	return -1;
 
-  // Get the encoder for spff
-  AVCodec *spffCodec = avcodec_find_encoder_by_name(AV_CODEC_ID_SPFF);
-  if(!spffCodec)
-    return -1;
+      //initiallize packet
+      av_init_packet(&imgPack);
+      imgPack.data = NULL;
+      imgPack.size = 0;
+      
+      // give the values from the AVFrame to the codec context
+      spffContext->pix_fmt = vFrameRGB->format;
+      spffContext->height = vFrameRGB->height;
+      spffContext->width = vFrameRGB->width;
   
-  // create a codec context spff
-  AVCodecContext *spffContext = avcodec_alloc_context3(spffCodec);
-  if(!spffContext)
-    return -1;
+      // get the encoder from frame
+      avcodec_send_frame(spffContext, vFrameRGB);
 
-  // give the values from the AVFrame to the codec context
-  spffContext->pix_fmt = vFrameRGB->pix_fmt;
-  spffContext->height = vFrameRGB->height;
-  spffContext->width = vFrameRGB->width;
+      // get a packet from spff and place it in imgPack
+      if(avcodec_receive_packet(spffContext, &imgPack) != 0)
+	{
+	  printf("Could not find the packet");
+	  return -1;
+	}
 
-  // get the encoder from frame
-  avcodec_send_frame(spffContext, vFrameRGB);
+      //av packet dump
+      file = fopen("frame%d.spff", "wb");
+      fwrite(imgPack.data, 1, imgPack.size, file);
 
-
-  // get a packet from spff and place it in imgPack
-  avcodec_recieve_packet(spffContext, &imgPack);
-
-  //av packet dump
   //av_pkt_dump2(FILE f, const AVPacket* pkt, int dump payload (dont matter = 0),Const AVStream* st)
   // file stream point where the dump should be sent to
   // st AVStream the packet belongs to
 
   
-  /* 
+      //Free the RGB image
+      av_free(buffer);
+      av_free(vFrameRGB);
+  
+      //Free the YUV frame
+      av_free(vFrame);
+
+      //Close the codecs
+      avcodec_close(imgCodex);
+      avcodec_close(spffContext);
+      
+      //Close the video file
+      avformat_close_input(&inContext);
+    }
+  return 0;
+}
+
+ /* 
   A visual reminder of what is declared
 
- AVPacket imgPack;
+  AVPacket imgPack;
   AVFormatContext *inContext;
   AVCodecContext *imgCodex;
   AVCodec *vCodec;
   AVFrame *vFrame;
   AVFrame *vFrameRGB;
-  */
-  return 0;
-}
+ */
