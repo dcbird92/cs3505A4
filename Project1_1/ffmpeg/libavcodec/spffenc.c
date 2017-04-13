4b93ac925b700c199bc8e16105530c34d807b208
msorger <msorger@lab2-23.eng.utah.edu>
2017-04-13 00:32:53 -0600

    Now using PJ's files and ball bouncing

--- Project1_1/ffmpeg/libavcodec/spffenc.c
+++ Project1_1/ffmpeg/libavcodec/spffenc.c
@@ -1,102 +1,112 @@
 /*
- * Michael Sorger and Sean Hammond
- * SPFF image format encoder
- * Copyright (c) 2006, 2007 Michel Bardiaux
- * Copyright (c) 2009 Daniel Verkamp <daniel at drv.nu>
- *
- * This file is part of FFmpeg.
- *
- * FFmpeg is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * FFmpeg is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Peter's codec encoder - high quality (RGB24), no compression.
  */
 
 #include "libavutil/imgutils.h"
 #include "libavutil/avassert.h"
 #include "avcodec.h"
 #include "bytestream.h"
-#include "spff.h"
 #include "internal.h"
 
 static av_cold int spff_encode_init(AVCodecContext *avctx)
 {
-  avctx->bits_per_coded_sample = 8; //(msb) 2R 3G 3B (lsb)
+  // Make sure we are receiving the correct type of image.
+  
+  if (avctx->pix_fmt != AV_PIX_FMT_RGB24)
+  {
+    av_log(avctx, AV_LOG_ERROR, "Only RGB24 is supported\n");
+    return AVERROR(EINVAL);
+  }
+  
+  // Set up the context for receiving a picture.
+  
+  avctx->bits_per_coded_sample = 24;  // Specify rgb24
+
+  // Indicate success.
+  
   return 0;
 }
 
-static int spff_encode_frame(AVCodecContext *avctx, AVPacket *AVpkt,
-                            const AVFrame *pict, int *got_packet)
+
+static int spff_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
+                            const AVFrame *pic, int *got_packet)
 {
-    const AVFrame * const pointerToPicture = pict;
-    int n_bytes_image, n_bytes_per_row, n_bytes, headerSize, retInt;
-    int pad_bytes_per_row = 0;
-    int i;
-    int bit_count = avctx->bits_per_coded_sample;
-    uint8_t *dataPtr, *dataBuffer;
+    int result, x, y, b;
+    uint8_t *output_data;
+    
+    // Debugging variables.
+    
+    static int reported = 0;
 
-    av_assert1(bit_count == 8); //bits_per_coded_sample = 8 check
-    n_bytes_per_row = ((int64_t)avctx->width * (int64_t)bit_count + 7LL)/8; //find number of bytes per row
-    pad_bytes_per_row = (4 - n_bytes_per_row) & 3; //find number of pad bytes per row (max is 3 bytes)
-    n_bytes_image = avctx->height * (n_bytes_per_row + pad_bytes_per_row); //find number of bytes for the image
+    // Debugging message.
+    
+    if (!reported)
+    {
+      av_log(avctx, AV_LOG_INFO, "*** CS 3505:  Executing in spffenc.c\n");
+      av_log(avctx, AV_LOG_INFO, "*** CS 3505:  Codec by Peter Jensen ***\n");
+      reported = 1;
+    }
+    
+    // Allocate an output memory buffer.
 
-    // STRUCTURE for SPFFFILEHEADER
-    // and related pages.
-#define SIZE_SPFFHEADER 14
-#define SIZE_SPFFINFOHEADER 40
-    headerSize = SIZE_SPFFHEADER + SIZE_SPFFINFOHEADER; //calculate headerSize
-    n_bytes = n_bytes_image + headerSize; //sum of bytes of image and headerSize
-    if ((retInt = ff_alloc_packet2(avctx, AVpkt, n_bytes, 0)) < 0) //allocate data using AVpkt
-        return retInt;
-    dataBuffer = AVpkt->data; //*int buf = data (8 bit int)
-    bytestream_put_byte(&dataBuffer, 'S');                   // SPFFHEADER.bfType
-    bytestream_put_byte(&dataBuffer, 'P');                   // do.
-    bytestream_put_byte(&dataBuffer, 'F');
-    bytestream_put_byte(&dataBuffer, 'F');
-    bytestream_put_le32(&dataBuffer, n_bytes);               // SPFFHEADER.bfSize
-    bytestream_put_le32(&dataBuffer, headerSize);            // SPFFHEADER.bfOffBits
-    bytestream_put_le32(&dataBuffer, SIZE_SPFFINFOHEADER);   // SPFFINFOHEADER.biSize
-    bytestream_put_le32(&dataBuffer, avctx->width);          // SPFFINFOHEADER.biWidth
-    bytestream_put_le32(&dataBuffer, avctx->height);         // SPFFINFOHEADER.biHeight
-    bytestream_put_le16(&dataBuffer, 1);                     // SPFFINFOHEADER.biPlanes
-    bytestream_put_le16(&dataBuffer, bit_count);             // SPFFINFOHEADER.biBitCount
-    bytestream_put_le32(&dataBuffer, n_bytes_image);         // SPFFINFOHEADER.biSizeImage
+    
+    result = ff_alloc_packet2(avctx, pkt,
+			      12 + avctx->width * avctx->height * 3,
+			      12 + avctx->width * avctx->height * 3);
 
-    // SPFF files are currently bottom-to-top so we start from the end...
-    dataPtr = pointerToPicture->data[0] + (avctx->height - 1) * pointerToPicture->linesize[0]; //set pointer to byte line at the bottom of picture 
-    dataBuffer = AVpkt->data + headerSize; //set the dataBuffer to AVpacket's data + headersize
-    for(i = 0; i < avctx->height; i++) 
-      { 
-	memcpy(dataBuffer, dataPtr, n_bytes_per_row); //copies dataBuffer, dataPtr, and bytes per row into memory
-        dataBuffer += n_bytes_per_row; //dataBuffer increments for next pass
-        memset(dataBuffer, 0, pad_bytes_per_row); //fill block in memory with the dataBuffer and pad 
-        dataBuffer += pad_bytes_per_row; //dataBuffer adds the pad at the end for next pass
-        dataPtr -= pointerToPicture->linesize[0]; //assign the pointer to location to be picture pointer linesize - 1
-      }
+    if (result < 0)
+        return result;
 
-    AVpkt->flags |= AV_PKT_FLAG_KEY; //pkt->flags = pkt->flags | AV_PKT_FLAG_KEY (pkt contains keyframe, bool operation)
-    *got_packet = 1; //int pointer gets set to 1
+    output_data = pkt->data;
+
+    // Write the header.    
+
+    bytestream_put_byte(&output_data, 's');
+    bytestream_put_byte(&output_data, 'p');
+    bytestream_put_byte(&output_data, 'f');
+    bytestream_put_byte(&output_data, 'f');
+
+    // Meaningless byte to block students who are incorrectly using this format.
+    //   (Not relevant in 2017, but I'm leaving it and the other harmless
+    //   flaw in.)
+    bytestream_put_byte(&output_data, ((char) output_data));  
+    
+    bytestream_put_le32(&output_data, avctx->width);
+    bytestream_put_be32(&output_data, avctx->height);
+
+    // Write the bytes.
+    
+    for (y = 0; y < avctx->height; y++)
+      for (x = 0; x < avctx->width; x++)
+	for (b = 0; b < 3; b++)
+          bytestream_put_byte(&output_data, pic->data[0][y*pic->linesize[0]+x*3+b]);
+
+    // Cite: bmp - unknown why a single frame is marked as a key frame.
+
+    pkt->flags |= AV_PKT_FLAG_KEY;
+
+    *got_packet = 1;
     return 0;
 }
 
+
+static av_cold int spff_encode_close(AVCodecContext *avctx)
+{
+  // Indicate success.
+  
+  return 0;
+}
+
+
 AVCodec ff_spff_encoder = {
     .name           = "spff",
-    .long_name      = NULL_IF_CONFIG_SMALL("SPFF (A Project for CS 3505)"),
+    .long_name      = NULL_IF_CONFIG_SMALL("Peter's spff encoder"),
     .type           = AVMEDIA_TYPE_VIDEO,
     .id             = AV_CODEC_ID_SPFF,
     .init           = spff_encode_init,
     .encode2        = spff_encode_frame,
+    .close          = spff_encode_close,
     .pix_fmts       = (const enum AVPixelFormat[]){
-    AV_PIX_FMT_RGB8,
-    AV_PIX_FMT_NONE
+    AV_PIX_FMT_RGB24, AV_PIX_FMT_NONE
     },
 };
